598903e0bde490403f03daf7374e8bae6cfeece7
Alan Kirkham <kirkhamadobe@gmail.com>
2026-08-07 14:22:08 +0100

    [kirkham] feat: make Sky demo AJO-authorable

--- web/profile-viewer/site-clone-bc.js
+++ web/profile-viewer/site-clone-bc.js
@@ -100,6 +100,8 @@
   var ORG_ID = 'BF9C27AA6464801C0A495FD0@AdobeOrg';
   var EDGE_DEPLOYMENT = 'nld2';
   var IFRAME_ID = cfg('iframeId', 'siteCloneDemoSiteFrame');
+  var SAME_DOCUMENT = !!cfg('sameDocument', false);
+  var CONTENT_ROOT_ID = cfg('contentRootId', '');
   var IFRAME_INLINE_SECTION_ID = cfg('inlineSectionId', 'siteCloneBcInline');
   var IFRAME_MOUNT_SELECTOR = '#brand-concierge-mount';
   var IFRAME_INJECTED_MOUNT_SELECTOR = cfg('injectedMountSelector', '#siteCloneBcInline #brand-concierge-mount');
@@ -329,6 +331,7 @@
   }
 
   function getIframeDoc() {
+    if (SAME_DOCUMENT) return document;
     var frame = getSiteCloneFrame();
     if (!frame) return null;
     try {
@@ -339,6 +342,7 @@
   }
 
   function getFrameSrcPath() {
+    if (SAME_DOCUMENT) return DEFAULT_FRAME_SRC;
     var frame = getSiteCloneFrame();
     if (!frame) return '';
     var src = frame.getAttribute('src') || '';
@@ -442,6 +446,7 @@
   }
 
   async function ensureSnapshotFrame() {
+    if (SAME_DOCUMENT) return;
     var frame = getSiteCloneFrame();
     if (!frame) return;
     var current = getFrameSrcPath();
@@ -764,9 +769,19 @@
   function positionBcFrameHost(fullscreen) {
     var host = document.getElementById(FRAME_OVERLAY_HOST_ID);
     var frame = getSiteCloneFrame();
-    if (!host || !frame) return;
+    var contentRoot = SAME_DOCUMENT && CONTENT_ROOT_ID ? document.getElementById(CONTENT_ROOT_ID) : null;
+    if (!host || (!frame && !contentRoot)) return;
 
-    var frameRect = frame.getBoundingClientRect();
+    var frameRect = frame
+      ? frame.getBoundingClientRect()
+      : {
+          top: 0,
+          left: 0,
+          right: global.innerWidth,
+          bottom: global.innerHeight,
+          width: global.innerWidth,
+          height: global.innerHeight,
+        };
     var top = frameRect.top;
     var doc = getIframeDoc();
 
